966be80451da77dcc25512cbae4219139999711d
SaadioFongangRoosevelt <rooseveltsaadio237@gmail.com>
2026-04-29 20:48:17 +0100

    Structure du projet

---
+++ src/models/Employe.java
@@ -0,0 +1,4 @@
+package models;
+
+public class Employe extends Personne{
+}
---
+++ src/models/Receptionniste.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Receptionniste extends Employe{
+    public Receptionniste(){
+        //le code il va falloir ajouter aussi les getters et les setters
+    }
+}
