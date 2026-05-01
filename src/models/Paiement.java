6b08246da0db8753fa9dff6475cf7e18a5cea298
ewidaltayou <ewidalt@gmail.com>
2026-05-01 22:25:24 +0200

    ajout des classes Reservation et Paiement dans models

--- src/models/Paiement.java
+++ src/models/Paiement.java
@@ -1,4 +1,31 @@
 package models;
 
 public class Paiement {
+    private double montant;
+    private String date;
+    private Client client;
+    private Reservation reservation;    
+
+    public Paiement(double montant, String date, Client client, Reservation reservation) {
+        this.montant = montant;
+        this.date = date;
+        this.client = client;
+        this.reservation = reservation;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
 }
